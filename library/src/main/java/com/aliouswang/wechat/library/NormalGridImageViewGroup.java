30751bd2e348a0d9165061c0ffff5d436cba2cee
aliouswang <wsw622@126.com>
2017-02-07 18:12:11 +0800

    Init commit

--- library/src/main/java/com/aliouswang/wechat/library/NormalGridImageViewGroup.java
+++ library/src/main/java/com/aliouswang/wechat/library/NormalGridImageViewGroup.java
@@ -5,6 +5,9 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 /**
  * Created by aliouswang on 17/2/7.
@@ -34,6 +37,16 @@ public class NormalGridImageViewGroup extends AbstractGridImageViewGroup<ImageVi
 
     @Override
     protected void loadImage(ImageView childView, String imageUrl) {
-        Glide.with(getContext()).load(imageUrl).into(childView);
+        Glide.with(getContext()).load(imageUrl).listener(new RequestListener<String, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                return false;
+            }
+        }).into(childView);
     }
 }
